3dd6693bbc39f5e957a4a8bd49699fdd16999948
Melhem Fares <faresm0305@gmail.com>
2023-01-21 10:02:06 -0500

    first commit

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const bcrypt = require('bcryptjs/dist/bcrypt')
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required:[true, 'Please provide a name'],
+        minlength:3,
+        maxlength:20
+    },
+    email: {
+        type: String,
+        required:[true, 'Please provide an email'],
+        match:[
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        'Please provide a valid email'
+        ],
+        unique:true
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength:6,
+        // maxlength:16
+    }
+})
+
+UserSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password,salt)
+    next()
+})
+
+UserSchema.methods.createJWT = function () {
+    return jwt.sign({userId:this._id, name:this.name}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_LIFETIME
+    })
+}
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password)
+    return isMatch
+}
+
+module.exports = mongoose.model('User', UserSchema)
